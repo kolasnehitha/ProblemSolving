f7c5fef550d4e70ded93e3f907d2e786af2818ab
kolasnehitha <108329483+kolasnehitha@users.noreply.github.com>
2022-07-06 10:54:31 +0530

    Update week4.py
    Changed the diagonal elements to 0

--- src/week4/week4.py
+++ src/week4/week4.py
@@ -4,7 +4,7 @@ def checkLocation(startLocation, endLocation):
         return True
     else:
         return False
-Distance = [[1, 2, 3], [2, 4, 5], [3, 5, 6]]
+Distance = [[0, 2, 3], [2, 0, 5], [3, 5, 0]]
 def distance(startLocation, endLocation):
     xCordinate = int(LocationsList.index(startLocation))
     yCordinate = int(LocationsList.index(endLocation))
@@ -18,13 +18,12 @@ endLocation = input("Enter end Location")
 if not checkLocation(startLocation, endLocation):
     print("check entered locations")
 else:
-    if startLocation == endLocation:
-        print("Distance from start point to end point is 0")
+    routeDistance = distance(startLocation, endLocation)
+    print("Distance from start point to end point is ", routeDistance)
+    transport = int(input("Enter type of transport 1-> Two wheeler 2-> Four Wheeler"))
+    if transport == 1:
+    print(" Time estimation is ",twoWheelerEstimation(routeDistance))
     else:
-        routeDistance = distance(startLocation, endLocation)
-        print("Distance from start point to end point is ", routeDistance)
-        transport = int(input("Enter type of transport 1-> Two wheeler 2-> Four Wheeler"))
-        if transport == 1:
-           print(" Time estimation is ",twoWheelerEstimation(routeDistance))
-        else:
-           print(" Time estimation is ",fourWheelerEstimation(routeDistance))
+        print(" Time estimation is ",fourWheelerEstimation(routeDistance))
+
+        
